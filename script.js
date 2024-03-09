3a762cb18f4ac9edf642ed33df978b501fd46186
Ryan Morgan <RyanOnGithub@gmail.com>
2024-03-09 10:52:38 -0800

    Add display price and display remaining cash in register functionality

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
 const userInput = document.getElementById('cash');
 const purchaseBtn = document.getElementById('purchase-btn');
+const priceDisplay = document.getElementById('price-display');
+const changeDisplay = document.getElementById('change-display');
+
+let price = 1.87;
+let cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+];
+
+const denominations = [
+  "Pennies",
+  "Nickels",
+  "Dimes",
+  "Quarters",
+  "Ones",
+  "Fives",
+  "Tens",
+  "Twenties",
+  "Hundreds"
+];
+
+const displayCashInDrawer = () => {
+  changeDisplay.innerHTML = ``;
+  
+  let idx = 0;
+  for (const denomination of denominations) {
+    changeDisplay.innerHTML += `<li>${denomination}: $${cid[idx][1]}</li>`;
+    idx++;
+    console.log('here');
+  }
+}
+
+const displayPrice = (price) => {
+  priceDisplay.textContent = `$${price}`
+}
+
+displayPrice(price);
+displayCashInDrawer();
